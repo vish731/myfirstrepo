3b50ec36e4cf0acb2f4030446006133a1b7eb9f6
Gojo <vishalvc16vc@gmail.com>
2026-02-23 23:23:08 +0530

    Add introductory C program with personal details

---
+++ intro.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    char name[] = "Gojo";
+    int age = 21;
+
+    printf("--- Introduction ---\n");
+    printf("My name is %s and I am %d years old.\n", name, age);
+    printf("Nice to meet you! 🚀\n");
+
+    return 0;
+}
